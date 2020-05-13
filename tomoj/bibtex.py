297cdbb6b39790e4eb7a2662d90d538aa41a637e
fede-pe <fepe.ig@gmail.com>
2020-05-13 11:06:58 +0200

    first files in plugin creation

---
+++ tomoj/bibtex.py
@@ -0,0 +1,67 @@
+# coding: latin-1
+# **************************************************************************
+# *
+# * Authors:     J.M. De la Rosa Trevin (delarosatrevin@scilifelab.se) [1]
+# *
+# * [1] SciLifeLab, Stockholm University
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+"""
+@article{MessaoudiI2007,
+title = "TomoJ: Tomography Software for Three-Dimensional Reconstruction in Transmission Electron Microscopy",
+journal = "BMC Bioinformatics",
+volume = "8",
+number = "288",
+year = "2007",
+note = "Electron Tomography",
+issn = "1471-2105",
+doi = "https://doi.org/10.1186/1471-2105-8-288",
+url = "https://bmcbioinformatics.biomedcentral.com/articles/10.1186/1471-2105-8-288",
+author = "Cédric MessaoudiI, Thomas Boudier, Carlos Oscar Sanchez Sorzano & Sergio Marco",
+keywords = "Tilt Angle, Fiducial Marker, Tomographic Reconstruction, Algebraic Reconstruction Technique, Tilt Axis",
+abstract = "Transmission electron tomography is an increasingly common three-dimensional electron microscopy approach
+that can provide new insights into the structure of subcellular components. Transmission electron tomography fills the
+gap between high resolution structural methods (X-ray diffraction or nuclear magnetic resonance) and optical microscopy.
+ We developed new software for transmission electron tomography, TomoJ. TomoJ is a plug-in for the now standard image
+ analysis and processing software for optical microscopy, ImageJ."
+}
+
+@article{Sorzano2009,
+title = "Marker-free image registration of electron tomography tilt-series",
+journal = "BMC Bioinformatics",
+volume = "10",
+number = "124",
+year = "2009",
+note = "Electron Tomography",
+issn = "1471-2105",
+doi = "https://doi.org/10.1186/1471-2105-10-124",
+url = "https://bmcbioinformatics.biomedcentral.com/articles/10.1186/1471-2105-10-124",
+author = "Carlos Oscar Sanchez Sorzano, Cédric Messaoudi, Matthias Eibauer, JR Bilbao-Castro, R Hegerl, S Nickell,
+S Marco & JM Carazo ",
+keywords = "Affine Transformation, Fiducial Marker, Robust Regression, Goal Function, Alignment Parameter",
+abstract = "Tilt series are commonly used in electron tomography as a means of collecting three-dimensional information
+from two-dimensional projections. A common problem encountered is the projection alignment prior to 3D reconstruction.
+Current alignment techniques usually employ gold particles or image derived markers to correctly align the images.
+When these markers are not present, correlation between adjacent views is used to align them. However, sequential
+pairwise correlation is prone to bias and the resulting alignment is not always optimal."
+}
+
+"""
